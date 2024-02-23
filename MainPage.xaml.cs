765251da08ac360f7973d11a039f578ca687e81e
Kristine Angeli Basilio <92997362+tinebasilio@users.noreply.github.com>
2024-02-23 15:36:24 +0800

    Add project files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnImagePageClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ImagePage());
+        }
+        private async void OnStackLayoutClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity2());
+        }
+        private async void OnGridActivityClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity3());
+        }
+
+    }
+}
